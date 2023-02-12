83028d1b32a79991cb0a52a734e09ebe26efec66
keitannunes <keitannunes@gmail.com>
2023-02-12 05:32:08 -0500

    Added QOL features

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+if (typeof(sessionStorage.getItem("keyWords")) !== 'undefined') {
+    document.getElementById("keywords-input").value = sessionStorage.getItem("keyWords"); 
+}
+
+const input = document.getElementById("keywords-input");
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("input-submit").click();
+    }
+  }); 
+
 const onHomeSubmit = async () => {
     document.getElementById("input-submit").textContent = "Loading..."; 
-    const val = document.getElementById("keywords-input").value; 
-    const ideas = await generateIdeas(val);
+    const keyWords = document.getElementById("keywords-input").value; 
+    const ideas = await generateIdeas(keyWords);
     let ideasStr = "";
     for (let i in ideas) {
         ideasStr += `${ideas[i]}|`
     }
     sessionStorage.setItem("optionList", ideasStr);
+    sessionStorage.setItem("keyWords", keyWords);
+    
     location.replace("options.html");
 }
--- options.js
+++ options.js
@@ -12,3 +12,18 @@ const onOptionsSubmit = async () => {
     }
     location.replace("results.html");
 }
+
+const reGenerateIdeas = async () => {
+    for (let i = 0; i < 5; i++) {
+        document.getElementById(`option${i}`).textContent = "Loading...";
+    }
+    generateIdeas(sessionStorage.getItem("keyWords")).then((newIdeas) => {
+        ideasStr = "";
+        for (let i = 0; i < 5; i++) {
+            ideas[i] = newIdeas[i];
+            ideasStr += `${ideas[i]}|`
+            document.getElementById(`option${i}`).textContent = ideas[i];
+        }
+        sessionStorage.setItem("optionList", ideasStr);
+    });
+}
